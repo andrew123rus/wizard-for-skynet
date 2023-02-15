e0ed56ea9607e014994177a20a9a45c4102bb957
andrew123rus <105133093+andrew123rus@users.noreply.github.com>
2023-02-16 01:27:32 +0300

    Update info.ts
    add try/catch in async function

--- src/store/info.ts
+++ src/store/info.ts
@@ -5,9 +5,14 @@ export const useInfoStore = defineStore('info', () =>
 {
    const dataforjson = ref<orderItems[]>([])
    async function getData() {
-      dataforjson.value = await fetch('https://cors-anywhere.herokuapp.com/https://skynet.ru/job/frontend/data.json')
-      .then((response) => response.json() as Promise<orderItems[]>) 
-   }
+      try {
+         const response = await fetch('https://cors-anywhere.herokuapp.com/https://skynet.ru/job/frontend/data.json');
+         const data:orderItems[] = await response.json();
+         dataforjson.value = data
+      } catch(err) {
+         console.log(err);
+      }
+    }
     const activeIndex=ref<number>(0)
     function changeIndex (val:number) {
         activeIndex.value = val
